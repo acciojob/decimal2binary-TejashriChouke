ae8e5656b3ddb54523ea942b83545b55b9b8e053
TejashriChouke <139746868+TejashriChouke@users.noreply.github.com>
2023-09-11 22:59:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function decimalToBinary(num) {
-  //Write you code here
+function decimalToBinary(decimalNumber) {
+  if (decimalNumber === 0) {
+    return '0';
+  }
+
+  let binary = '';
+  while (decimalNumber > 0) {
+    // Append the remainder when dividing by 2 to the binary string
+    binary = (decimalNumber % 2) + binary;
+    // Divide the number by 2 and take the integer part
+    decimalNumber = Math.floor(decimalNumber / 2);
+  }
+
+  return binary;
   
 }
 
